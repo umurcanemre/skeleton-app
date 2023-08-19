be23f6b335986924c1cf4d92871bddcc8f633982
umurcan emre <5785630+umurcanemre@users.noreply.github.com>
2023-08-19 14:35:58 +0200

    Anagram checking alternative case coverage added

--- src/test/kotlin/com/umurcan/skeletonapp/service/AnagramCheckerTest.kt
+++ src/test/kotlin/com/umurcan/skeletonapp/service/AnagramCheckerTest.kt
@@ -1,6 +1,8 @@
 package com.umurcan.skeletonapp.service
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -14,6 +16,24 @@ class AnagramCheckerTest {
         assertTrue(target.isAnagram(word1, word2))
     }
 
+    @Test
+    fun `should return true on empty words`() {
+        assertTrue(target.isAnagram("      ".toCharArray(), "".toCharArray()))
+        assertTrue(target.isAnagram("".toCharArray(), "".toCharArray()))
+    }
+
+    @ParameterizedTest
+    @MethodSource("international_anagrams")
+    fun `should return true on special character anagrams`(word1: CharArray, word2: CharArray) {
+        assertTrue(target.isAnagram(word1, word2))
+    }
+
+    @ParameterizedTest
+    @MethodSource("non-anagrams")
+    fun `should return false on non-anagrams`(word1: CharArray, word2: CharArray) {
+        assertFalse(target.isAnagram(word1, word2))
+    }
+
 
     companion object {
         @JvmStatic
@@ -24,6 +44,23 @@ class AnagramCheckerTest {
                 Arguments.of("forty five".toCharArray(), "over fifty".toCharArray()),
                 Arguments.of("William Shakespeare".toCharArray(), "I am a weakish speller".toCharArray()),
                 Arguments.of("Tom Marvolo Riddle".toCharArray(), "I am Lord Voldemort".toCharArray()),
+        )
+        @JvmStatic
+        fun international_anagrams() = listOf(
+                Arguments.of("Fußgängerübergänge".toCharArray(), "berg F gerüÄnge ußgän".toCharArray()),
+                Arguments.of("पैदल यात्री क्रॉसिंग".toCharArray(), "पैदल क्रॉसिंग   यात्री".toCharArray()),
+                Arguments.of("人行横道".toCharArray(), "  人行道  横".toCharArray()),
+                Arguments.of("横断歩道".toCharArray(), "横断 道 歩".toCharArray()),
+                Arguments.of("çığ düşüyör".toCharArray(), "ığç üyör düş".toCharArray()),
+        )
+        @JvmStatic
+        fun `non-anagrams`() = listOf(
+                Arguments.of("anagrams".toCharArray(), "    ".toCharArray()),
+                Arguments.of("New York Timess".toCharArray(), "monkeys write".toCharArray()),
+                Arguments.of("evil".toCharArray(), "ville".toCharArray()),
+                Arguments.of("forty    five".toCharArray(), "ower fifty".toCharArray()),
+                Arguments.of("William Shakespeare".toCharArray(), "I am a strong speller".toCharArray()),
+                Arguments.of("Tom Marvolo Riddle".toCharArray(), "I am Mord Voldemort".toCharArray()),
         )
     }
 }
